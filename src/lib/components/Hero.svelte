71ccfea23a69868479539faff63bbffac159c16d
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-06 11:05:06 -0700

    updated hero section

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,48 +1,8 @@
-<main class="h-screen flex">
-  <section class="w-1/2 h-full bg-white flex items-center justify-center text-gray-800">
-    <div>
-      <h1 class="text-8xl font-bold">HELLO</h1>
-      <h1 class="text-7xl font-bold">HELLO</h1>
-      <h1 class="text-6xl font-bold">HELLO</h1>
-    </div>
-  </section>
-  <section class="w-1/2 h-full bg-yellow-800/40 flex items-center justify-center">
-    <img class="w-1/2" src="images/pawel-czerwinski-Qiy4hr18aGs-unsplash.jpg" alt="">
-  </section>
+<main class="relative flex h-full flex-col items-center justify-center">
+  <video muted autoplay loop playsinline disablepictureinpicture>
+    <source src="/images/hero-video.mp4" type="video/webm">
+  </video>
+  <section class="animate-typing absolute top-8 left-0 text-5xl px-4 py-2 font-rasa md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200">Trusted by the most innovative<br>minds</section>
 </main>
-
-<style>
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-
-  .animated-section {
-    animation: fadeIn 1s ease-in-out, spin 2s linear infinite;
-  }
-</style>
-
-<section class="flex items-center justify-center h-screen bg-blue-700 text-white">
-  <h1 class="text-8xl">Words Words Words</h1>
-</section>
-
-<section class="flex items-center justify-center h-screen bg-pink-700 text-white animated-section">
-  <h1 class="text-8xl">Words Words Words</h1>
-</section>
-
-
 
 
