355c5052a97221a283bebeccbf6e4f6f437796a3
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-06 21:30:13 -0700

    added comments to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,8 @@
 <script>
+  // Import Icon component from iconify
   import Icon from '@iconify/svelte';
-
+ 
+  // Navigation array
   const navItems = [
     { name: 'Home', href: '/' },
     { name: 'About', href: '/about' },
@@ -9,33 +11,36 @@
 </script>
 
 <footer class="bg-black p-4 text-white rounded-md shadow-lg">
-<nav>
-  <ul class="flex justify-center gap-9 font-barlow">
-    {#each navItems as { name, href }}
-      <li>
-        <a href={href} class="text-xl hover:text-indigo-400 hover:underline transition-colors duration-300">{name}</a>
-      </li>
-    {/each}
-  </ul>
-</nav>
-
-<p class="text-center">&copy; Jessica Nelson</p>
-
-<section class="flex items-center justify-center gap-8 text-4xl p-4">
-  <a href="https://www.instagram.com/jessicamnelson_/" class="text-white hover:text-gray-400 transition">
+  <!-- Navigation section -->
+  <nav>
+    <ul class="flex justify-center gap-9 font-barlow">
+      <!-- Iterate over each navigation item in the array -->
+      {#each navItems as { name, href }}
+        <li>
+          <a href={href} class="text-xl hover:text-indigo-400 hover:underline transition-colors duration-300">{name}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+  <!-- Copyright -->
+  <p class="text-center">&copy; Jessica Nelson</p>
+  
+  <!-- Section with social media links -->
+  <section class="flex items-center justify-center gap-8 text-4xl p-4">
+    <a href="https://www.instagram.com/jessicamnelson_/" class="text-white hover:text-gray-400 transition">
       <p><Icon icon="mdi:instagram" class="h-12 w-12" /></p>
-  </a>
+    </a>
 
-  <a href="https://github.com/jessicamnelsonn" class="text-white hover:text-gray-400 transition">
+    <a href="https://github.com/jessicamnelsonn" class="text-white hover:text-gray-400 transition">
       <p><Icon icon="mdi:github" class="h-12 w-12" /></p>
-  </a>
+    </a>
 
-  <a href="https://www.linkedin.com/in/jessicamnelson" class="text-white hover:text-gray-400 transition">
+    <a href="https://www.linkedin.com/in/jessicamnelson" class="text-white hover:text-gray-400 transition">
       <p><Icon icon="mdi:linkedin" class="h-12 w-12" /></p>
-  </a>
-</section>
-
+    </a>
+  </section>
 </footer>
+
 
 
 
