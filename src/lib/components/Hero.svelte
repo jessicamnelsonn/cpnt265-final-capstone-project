6d1fb2b488ccc8e3c7946df7f4d1b43d7d409360
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-07 22:38:51 -0700

    Finished about me section, added responsiveness and added comments

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,68 +1,100 @@
 <script>
-	import { onMount, onDestroy } from 'svelte';
-	import { slide } from 'svelte/transition';
-	
-	let greetings = ['Hello', 'Hola',];
-	let index = 0;
-	let roller;
-	
-	onMount(() => {
-		roller = setInterval(() => {
-			if (index === greetings.length - 1) index = 0;
-			else index++;
-		}, 1250);
-	});
-	
-	onDestroy(() => {
-		clearInterval(roller);
-	});
+  // Importing necessary functions from the Svelte library
+  import { onMount, onDestroy } from 'svelte';
+  import { slide } from 'svelte/transition';
+
+  // Initializing variables
+  let greetings = ['Hello', 'Hola'];
+  let index = 0;
+  let roller;
+
+  // Code to be executed when the component is mounted
+  onMount(() => {
+    // Setting up an interval to cycle through greetings
+    roller = setInterval(() => {
+      // Check if the index is at the end of the greetings array, then reset to the beginning
+      if (index === greetings.length - 1) index = 0;
+      else index++;
+    }, 1250);
+  });
+
+  // Code to be executed when the component is destroyed
+  onDestroy(() => {
+    // Clearing the interval to prevent memory leaks
+    clearInterval(roller);
+  });
 </script>
 
+<!-- Hero Section -->
 <main class="relative flex h-full flex-col items-center justify-center">
-  <video muted autoplay loop playsinline disablepictureinpicture>
+  <!-- Background video -->
+  <video class="w-full h-full object-cover" muted autoplay loop playsinline disablepictureinpicture>
     <source src="/images/hero-video.mp4" type="video/mp4">
   </video>
-  <section>
-  <h1 class="absolute top-8 left-0 text-6xl px-4 py-2 font-barlow md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200">Trusted by the most innovative minds</h1>
-</section>
+
+  <!-- Text section over the video -->
+  <section class="absolute top-1/3 left-0 transform -translate-y-2/5">
+    <!-- Dynamic greeting text with a gradient background -->
+    <h1 class="text-3xl md:text-5xl px-4 py-2 font-barlow text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200 bg-clip-text">
+      Trusted by the most innovative minds
+    </h1>
+  </section>
 </main>
 
-<main class="h-screen flex">
-  <section class="w-1/2 h-full bg-white text-black p-4">
-      <div>
-          <h1 class="text-8xl font-bold text-center">About</h1>
-          
-          <p>My name is Jessica Nelson, a passionate individual with a vibrant mix of Mexican and Canadian roots. I embrace the richness of my cultural background, allowing me to seamlessly navigate conversations in both English and Spanish.
-            
-          <h2 class="text-3xl">Passion for Visual Storytelling</h2>
-            
-          <p>I've always been captivated by the power of visual storytelling. My journey into the world of videography and photography began with a fascination for capturing moments and turning them into narratives. Through the lens of my camera, I aim to convey emotions, share experiences, and celebrate the beauty of diversity.</p>  
-            
-          <h2 class="text-3xl">Bridging Cultures Through Language</h2>
-            
-           <p>Being bilingual in Spanish and English is more than just a skill for me; it's a bridge that connects me to the stories and experiences of people from different backgrounds. I find joy in breaking down language barriers and fostering understanding.</p> 
-            
-          <h2 class="text-3xl">Web Development Journey</h2>
-            
-           <p>Currently, I'm on an exciting journey, finishing up a certificate program in web development. This adventure allows me to blend my creative passion with technical skills. I find satisfaction in crafting digital experiences that not only look stunning but also function seamlessly.</p> 
-            
-          <h2 class="text-3xl">What Drives Me</h2>
-            
-           <p>Whether I'm behind the camera or coding a website, my goal remains the same: to create meaningful connections. I believe that technology, combined with art, has the power to bring people together and amplify voices that might otherwise go unheard.</p> 
-            
-          <p>Join me on this journey as I continue to learn, grow, and contribute my unique perspective to the ever-evolving landscape of visual storytelling and web development.</p>
-            
-          <p>Let's create something extraordinary together!</p>
-    
-      </div>
-  </section>
-  <section class="w-1/2 h-full bg-black flex items-center justify-center">
+<!-- About Section -->
+<main class="h-screen flex flex-col md:flex-row">
+  <!-- Left Section with Greetings -->
+  <section class="w-full md:w-1/2 h-full bg-white text-black flex items-center justify-center">
     <div>
       {#key index}
-        <h1 class="text-white text-8xl text-center" transition:slide>{greetings[index]}!</h1>
+        <!-- Displaying dynamic greetings with a sliding transition -->
+        <h1 class="text-black text-8xl text-center" transition:slide>{greetings[index]}!</h1>
       {/key}
     </div>
   </section>
+
+  <!-- Right Section with About Me Content -->
+  <section class="w-full md:w-1/2 h-full bg-black text-white p-4 overflow-auto">
+    <div>
+      <!-- Main heading for the About Me section -->
+      <h1 class="text-4xl md:text-6xl font-bold text-center mb-6">About Me</h1>
+
+      <!-- Paragraphs providing information about me -->
+      <p class="text-base md:text-lg leading-loose">
+        My name is Jessica Nelson, a passionate individual with a vibrant mix of Mexican and Canadian roots. I embrace the richness of my cultural background, allowing me to seamlessly navigate conversations in both English and Spanish.
+      </p>
+
+      <h2 class="text-xl md:text-2xl mt-6 mb-2">Passion for Visual Storytelling</h2>
+
+      <p class="text-base md:text-lg leading-loose">
+        I've always been captivated by the power of visual storytelling. My journey into the world of videography and photography began with a fascination for capturing moments and turning them into narratives. Through the lens of my camera, I aim to convey emotions, share experiences, and celebrate the beauty of diversity.
+      </p>
+
+      <h2 class="text-xl md:text-2xl mt-6 mb-2">Bridging Cultures Through Language</h2>
+
+      <p class="text-base md:text-lg leading-loose">
+        Being bilingual in Spanish and English is more than just a skill for me; it's a bridge that connects me to the stories and experiences of people from different backgrounds. I find joy in breaking down language barriers and fostering understanding.
+      </p>
+
+      <h2 class="text-xl md:text-2xl mt-6 mb-2">Web Development Journey</h2>
+
+      <p class="text-base md:text-lg leading-loose">
+        Currently, I'm on an exciting journey, finishing up a certificate program in web development. This adventure allows me to blend my creative passion with technical skills. I find satisfaction in crafting digital experiences that not only look stunning but also function seamlessly.
+      </p>
+
+      <h2 class="text-xl md:text-2xl mt-6 mb-2">What Drives Me</h2>
+
+      <p class="text-base md:text-lg leading-loose">
+        Whether I'm behind the camera or coding a website, my goal remains the same: to create meaningful connections. I believe that technology, combined with art, has the power to bring people together and amplify voices that might otherwise go unheard.
+      </p>
+
+      <p class="text-base md:text-lg leading-loose mt-4">
+        Join me on this journey as I continue to learn, grow, and contribute my unique perspective to the ever-evolving landscape of visual storytelling and web development. Let's create something extraordinary together!
+      </p>
+    </div>
+  </section>
 </main>
+
+
 
 
