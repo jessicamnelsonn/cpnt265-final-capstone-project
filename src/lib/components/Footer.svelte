1458f84fabc1967381004837a16f246fcb971a49
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-11 11:02:37 -0700

    changed footer layout on mobile

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -28,7 +28,7 @@
   <p class="text-center mt-4">&copy; Jessica Nelson</p>
   
   <!-- Section with social media links -->
-  <section class="flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-8 text-4xl p-4">
+  <section class="flex flex-row items-center justify-center gap-4 sm:gap-8 text-4xl p-4">
     <a href="https://www.instagram.com/jessicamnelson_/" class="text-white hover:text-gray-400 transition">
       <p><Icon icon="mdi:instagram" class="h-12 w-12" /></p>
     </a>
