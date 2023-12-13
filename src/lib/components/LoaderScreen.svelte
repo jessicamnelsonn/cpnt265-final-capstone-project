27030bd32e5719b1f12319a32602b2662c17ecfc
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-12 18:27:13 -0700

    fixed loader screen

--- src/lib/components/LoaderScreen.svelte
+++ src/lib/components/LoaderScreen.svelte
@@ -2,26 +2,23 @@
   import { onMount } from 'svelte';
 
   onMount(() => {
-    // Use window.onload to ensure the entire page is loaded
-    window.onload = () => {
-      // Find the loader wrapper element
-      const loaderWrapper = document.querySelector('.loader-wrapper');
+    // Find the loader wrapper element
+    const loaderWrapper = document.querySelector('.loader-wrapper');
 
-      // Check if the loader wrapper element exists
-      if (loaderWrapper) {
-        // Use setTimeout to delay hiding the loader by changing its display style
-        setTimeout(() => {
-          loaderWrapper.style.display = 'none';
-        }, 1500);
-      } else {
-        console.error('Loader wrapper not found');
-      }
-    };
+    // Check if the loader wrapper element exists
+    if (loaderWrapper) {
+      // Use setTimeout to delay hiding the loader by changing its display style
+      setTimeout(() => {
+        loaderWrapper.style.display = 'none';
+      }, 1500);
+    } else {
+      console.error('Loader wrapper not found');
+    }
   });
 </script>
 
 <!-- Loader wrapper styled -->
-<section class="loader-wrapper absolute w-screen h-screen bg-black flex justify-center items-center top-0 left-0">
+<section class="loader-wrapper fixed inset-0 bg-black flex justify-center items-center">
   <!-- Loader image -->
   <img src="/images/loader-screen.gif" alt="astronaut in space gif">
 </section>
